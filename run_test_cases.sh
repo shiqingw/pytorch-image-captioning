f8cc5c8453df5b80606d49772be59fc168c6bc70
Shiqing Wei <shiqing.wei@outlook.com>
2022-12-17 10:04:41 -0500

    run exp_009-010

--- run_test_cases.sh
+++ run_test_cases.sh
@@ -1,8 +1,4 @@
-#python3 -u main.py --exp_num 5 > results/exp_005/output.out
-#python3 -u main.py --exp_num 6 > results/exp_006/output.out
-#python3 -u main.py --exp_num 7 > results/exp_007/output.out
-#python3 -u main.py --exp_num 8 > results/exp_008/output.out
-python3 -u draw_samples.py --exp_num 5
-python3 -u draw_samples.py --exp_num 6
-python3 -u draw_samples.py --exp_num 7
-python3 -u draw_samples.py --exp_num 8
+python3 -u main.py --exp_num 9 > results/exp_009/output.out
+python3 -u main.py --exp_num 10 > results/exp_010/output.out
+python3 -u draw_samples.py --exp_num 9
+python3 -u draw_samples.py --exp_num 10
