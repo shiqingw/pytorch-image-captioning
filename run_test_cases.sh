de7a433e8c1621dfbb407846e6a1b9089a7f7287
Shiqing Wei <shiqing.wei@outlook.com>
2022-12-18 00:00:58 -0500

    run

--- run_test_cases.sh
+++ run_test_cases.sh
@@ -1,10 +1,27 @@
+python3 -u main.py --exp_num 1 > results/exp_001/output.out
+python3 -u main.py --exp_num 2 > results/exp_002/output.out
+python3 -u main.py --exp_num 3 > results/exp_003/output.out
+python3 -u main.py --exp_num 4 > results/exp_004/output.out
+python3 -u main.py --exp_num 5 > results/exp_005/output.out
+python3 -u main.py --exp_num 6 > results/exp_006/output.out
+python3 -u main.py --exp_num 7 > results/exp_007/output.out
+python3 -u main.py --exp_num 8 > results/exp_008/output.out
+python3 -u main.py --exp_num 9 > results/exp_009/output.out
+python3 -u main.py --exp_num 10 > results/exp_010/output.out
 python3 -u main.py --exp_num 11 > results/exp_011/output.out
+python3 -u main.py --exp_num 12 > results/exp_013/output.out
 python3 -u main.py --exp_num 13 > results/exp_013/output.out
-python3 -u main.py --exp_num 14 > results/exp_014/output.out
-python3 -u main.py --exp_num 15 > results/exp_015/output.out
-python3 -u main.py --exp_num 16 > results/exp_016/output.out
+python3 -u draw_samples.py --exp_num 1
+python3 -u draw_samples.py --exp_num 2
+python3 -u draw_samples.py --exp_num 3
+python3 -u draw_samples.py --exp_num 4
+python3 -u draw_samples.py --exp_num 5
+python3 -u draw_samples.py --exp_num 6
+python3 -u draw_samples.py --exp_num 7
+python3 -u draw_samples.py --exp_num 8
+python3 -u draw_samples.py --exp_num 9
+python3 -u draw_samples.py --exp_num 10
 python3 -u draw_samples.py --exp_num 11
+python3 -u draw_samples.py --exp_num 12
 python3 -u draw_samples.py --exp_num 13
-python3 -u draw_samples.py --exp_num 14
-python3 -u draw_samples.py --exp_num 15
-python3 -u draw_samples.py --exp_num 16
+
